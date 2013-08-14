8e657f5204bb45ddd17d8e90d2773d915052d140
nodebench <nodebench@gmail.com>
2013-08-14 10:49:01 +0530

    Instances for GROUP and FRAMES added. Motion blur for instances included.

--- sfrsTest/src/sfrsMinimal/Instances.py
+++ sfrsTest/src/sfrsMinimal/Instances.py
@@ -46,42 +46,58 @@ def getPos(obj, as_matrix=True):
     obj_mat = obj.matrix.copy()
     matrix_rows = [ "%+0.4f" % element for rows in obj_mat for element in rows ]
     return (matrix_rows)
-        
-def getMotionBlurMatrices(scene=None, camera=None, steps=0, as_matrix=True):
-    current_frame , current_subframe = (scene.frame_current, scene.frame_subframe)
-    mb_start = current_frame - math.ceil(steps / 2) + 1
-    frame_steps = [ mb_start + n for n in range(0, steps) ]
-    matrices = [] 
-    for sub_frame in frame_steps:
-        scene.frame_set(sub_frame, current_subframe)
-        obj = scene.objects['Camera']
-        matrices.append(getPos(obj, as_matrix))
-    scene.frame_set(current_frame, current_subframe)
-    return matrices
-                 
-
-def InstanceExporter(scene , objname, turn_on_motion_blur):
-    # TODO: motion blur related calculations
-    # current_frame = (scene.frame_current , scene.frame_subframe) 
     
-    obj_parent = scene.objects[objname]
-    obj_parent.dupli_list_create(scene)
-    dupli_list = {}
-    for obj in obj_parent.dupli_list :
-        ins = {}
-        pos = getPos(obj, as_matrix=True)   
-        ins['iname'] = "%s.inst.%03d" % (obj_parent.name, obj.index)
-        ins['index'] = obj.index
-        ins['pname'] = obj.object.name
-        ins['trans'] = [pos]
-        # TODO: try deepcopy here check any performance change
-        dupli_list[ ins['iname'] ] = ins
-    obj_parent.dupli_list_clear()
-    return dupli_list
-    
+def InstanceExporter(scene , objname, turn_on_motion_blur, steps=0):
+    if not turn_on_motion_blur:
+        obj_parent = scene.objects[objname]
+        obj_parent.dupli_list_create(scene)
+        dupli_list = {}
+        for obj in obj_parent.dupli_list :
+            ins = {}
+            pos = getPos(obj, as_matrix=True)   
+            ins['iname'] = "%s.inst.%03d" % (obj_parent.name, obj.index)
+            ins['index'] = obj.index
+            ins['pname'] = obj.object.name
+            ins['trans'] = [pos]
+            # TODO: try deepcopy here check any performance change
+            dupli_list[ ins['iname'] ] = ins
+        obj_parent.dupli_list_clear()
+        return dupli_list
+    else:
+        obj_parent = scene.objects[objname]
+        obj_parent.dupli_list_create(scene)
+        num_dupli = len(obj_parent.dupli_list)
+        dupli_list = {}
+        for obj in obj_parent.dupli_list :
+            ins = {}  
+            ins['iname'] = "%s.inst.%03d" % (obj_parent.name, obj.index)
+            ins['index'] = obj.index
+            ins['pname'] = obj.object.name
+            ins['trans'] = None
+            dupli_list[ ins['iname'] ] = ins
+        obj_parent.dupli_list_clear()
         
+        current_frame , current_subframe = (scene.frame_current, scene.frame_subframe)
+        mb_start = current_frame - math.ceil(steps / 2) + 1
+        frame_steps = [ mb_start + n for n in range(0, steps) ]
+        transform = [ [] for dummy_i in range(num_dupli)] 
+        for sub_frame in frame_steps:
+            scene.frame_set(sub_frame, current_subframe)            
+            obj_parent.dupli_list_create(scene)
+            for objindx in range(num_dupli) :
+                xpos = getPos(obj_parent.dupli_list[objindx], as_matrix=True)
+                transform[objindx].append(xpos)
+            obj_parent.dupli_list_clear()
+        scene.frame_set(current_frame, current_subframe)
 
-
+        for dup in dupli_list.values():
+            if obj_parent.dupli_type == 'FRAMES':
+                dup['trans'] = transform[dup['index'] - obj_parent.dupli_frames_start ]
+            else:
+                dup['trans'] = transform[dup['index']]            
+        return dupli_list
+        
+        
 def sceneObjectsExporter(ObjectsRepository={}, Export_instances=False):  
     # FIXME: we dont need this as a module
     # loop through the objects in the scene
@@ -92,10 +108,9 @@ def sceneObjectsExporter(ObjectsRepository={}, Export_instances=False):
     
     # filter objects - avoid camera , lamp
     obj_lst = [ obj.name  for obj in scene.objects if obj.type not in ['CAMERA', 'LAMP'] ]
-    print(obj_lst)
+
     
-    # filter objects - avoid meshlights ; these are not objects but lights
-    
+    # filter objects - avoid meshlights ; these are not objects but lights    
     nonlights = [obj for obj in obj_lst if obj not in ObjectsRepository['MeshLightObjects'].keys() ]
     del obj_lst
     obj_lst = nonlights
@@ -104,34 +119,40 @@ def sceneObjectsExporter(ObjectsRepository={}, Export_instances=False):
         proxy_list = {}
         
         for objname in obj_lst:
-            turn_on_motion_blur = False
+            turn_on_motion_blur = True
+            mblur_steps = 5
             if objname in ObjectsRepository['MotionBlurObjects'].keys() :
+                mblur_steps = 5
                 turn_on_motion_blur = True        
             cur_object = scene.objects[objname]
-            
-    #         print(cur_object)
-    #         print(cur_object.is_duplicator)
-    #         print(cur_object.children)
-    #         print(cur_object.dupli_type)
+
             
             if (
                 (cur_object.is_duplicator) & 
                 (cur_object.children != ()) & 
                 (cur_object.dupli_type in ['VERTS' , 'FACES' ]) 
                 ):
-                dupli_list = InstanceExporter(scene , objname , turn_on_motion_blur)
+                dupli_list = InstanceExporter(scene , objname , turn_on_motion_blur , mblur_steps)
+                dmix(ObjectsRepository, dupli_list, 'Instances')
+                proxy_list[objname] = True
+                print ("Instantiated>> %s" % objname)
+            if (
+                (cur_object.is_duplicator) & 
+                (cur_object.children == ()) & 
+                (cur_object.dupli_type in ['GROUP' , 'FRAMES']) 
+                ):
+                dupli_list = InstanceExporter(scene , objname , turn_on_motion_blur , mblur_steps)
                 dmix(ObjectsRepository, dupli_list, 'Instances')
                 proxy_list[objname] = True
                 print ("Instantiated>> %s" % objname)
             dmix(ObjectsRepository, proxy_list, 'Instantiated')
             
             
-        # filter objects - avoid instances ; 
-        
+        # filter objects - avoid instances ;         
         noninst = [obj for obj in obj_lst if obj not in ObjectsRepository['Instantiated'].keys() ]
         del obj_lst
         obj_lst = noninst 
-        print(obj_lst)   
+        # print(obj_lst)   
     
     for objname in obj_lst:
         # ActualsExporter()
@@ -166,7 +187,9 @@ def getListInstances():
     if key in ObjectsRepository.keys():
         # print (ObjectsRepository[key])
         for each in ObjectsRepository[key].items():
-            print (each)
+            # print (each)
+            pass
+            
             
     #===========================================================================
     
@@ -183,7 +206,15 @@ def getListInstances():
             indent += 1 
             act_inst.append("%s %s %s" % (space * indent , "name", inst['iname']))
             act_inst.append("%s %s %s" % (space * indent , "geometry", inst['pname']))
-            act_inst.append("%s %s %s" % (space * indent , "transform  row", ' '.join(inst['trans'][0])))
+            ln = len(inst['trans'])
+            if ln == 1:
+                act_inst.append("%s %s %s" % (space * indent , "transform  row", ' '.join(inst['trans'][0])))
+            else:
+                act_inst.append("%s %s %s" % (space * indent , "transform", ""))
+                act_inst.append("%s %s %s" % (space * indent , "steps", 5))
+                act_inst.append("%s %s %s" % (space * indent , "times", "0 1"))                
+                for exh in range(ln):
+                    act_inst.append("%s %s %s" % (space * indent , "row", ' '.join(inst['trans'][exh])))                
             act_inst.append("%s %s %s" % (space * indent , "shader", "Material.shader"))
             indent -= 1 
             act_inst.append("%s %s %s" % (space * indent , "}", ""))
@@ -193,19 +224,6 @@ def getListInstances():
         instfile.write("\n%s" % x)
     instfile.close()
     #===========================================================================
-
-
-# instance {
-#    name nameOfInstance
-#    geometry theOriginalObjectName
-#    transform {
-#       rotatex -90
-#       scaleu 1.0
-#       translate -1.0 3.0 -1.0
-#    }
-#    shader shaderForInstance
-#    modifier modForInstance
-# }
 
 
 if __name__ == '__main__':
